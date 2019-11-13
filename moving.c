28bf26685a5a0f935b56aa470a9ca1b0d85d8cb9
yurigalindo <yurioliveiragalindo@gmail.com>
2019-11-12 23:44:12 -0300

    controls models with arrow keys

---
+++ moving.c
@@ -0,0 +1,62 @@
+#include "pacman_sprites.c"
+
+#include <GL/glut.h>
+
+void init(){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
+    glMatrixMode(GL_PROJECTION); //define que a matrix é a de projeção
+    glLoadIdentity(); //carrega a matrix de identidade
+
+    gluPerspective(120.0, 1.0, 0.1, 100.0); //define uma projeção perspectiva
+    
+    glViewport(0, 0, 500, 500);
+
+}
+
+
+int posX,posY;
+void SpecialKeys (int key, int x, int y){
+   switch(key){
+      case GLUT_KEY_LEFT:
+         posX-=10;
+         break;
+      case GLUT_KEY_RIGHT:
+         posX+=10;
+         break;
+      case GLUT_KEY_UP:
+         posY+=10;
+         break;
+      case GLUT_KEY_DOWN:
+         posY-=10;
+         break;
+   }
+   glutPostRedisplay();
+}
+
+void World(){
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0,-3,20,
+              0.0,0.0,0.0,
+              0.0, 1.0, 0.0); //vetor view-up (V)  //vetor view-up
+	glPushMatrix();
+	glTranslatef(posX,posY,0);
+	pacman2();
+	glPopMatrix();
+
+}
+
+int main(int argc, char *argv[]){
+	glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowPosition(50, 50);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Pac-Man");
+
+    glutDisplayFunc(World);
+    glutSpecialFunc(SpecialKeys);
+    init();
+    
+    glutMainLoop();
+}
